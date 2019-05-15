38de64fdb93d28026f6a7f0a332d1116874652bd
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2019-05-15 17:23:17 +0100

    Change async test to use fakeAsync instead

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { TestBed, ComponentFixture, fakeAsync, tick, flush } from "@angular/core/testing";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { ActivatedRoute } from "@angular/router";
 import { HeroService } from "../hero.service";
@@ -39,15 +39,13 @@ describe('HeroDetailComponent', () => {
         expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
     });
 
-    it('should call updateHero when save is called', (done) => {
+    it('should call updateHero when save is called', fakeAsync(() => {
         mockHeroService.updateHero.and.returnValue(of({}));
         fixture.detectChanges();
 
         fixture.componentInstance.save();
+        flush();
 
-        setTimeout(() => {
-            expect(mockHeroService.updateHero).toHaveBeenCalled();
-            done();
-        }, 300);
-    });
+        expect(mockHeroService.updateHero).toHaveBeenCalled();
+    }));
 });
