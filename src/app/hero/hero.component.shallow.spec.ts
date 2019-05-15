557a7d3e341f42df7122f5bb6a66c82cc6594264
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2019-05-15 10:23:49 +0100

    Ignore unknown attributes and elements

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { HeroComponent } from "./hero.component";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 describe('HeroComponent (shallow tests)', () => {
     let fixture: ComponentFixture<HeroComponent>;
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [HeroComponent]
+            declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA]
         });
 
         fixture = TestBed.createComponent(HeroComponent);
