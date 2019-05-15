01646b297ac3a06c89fb578af659c12a0f07b5ec
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2019-05-15 11:03:59 +0100

    Add test to verify anchor tag set correctly

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { HeroComponent } from "./hero.component";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe('HeroComponent (shallow tests)', () => {
     let fixture: ComponentFixture<HeroComponent>;
@@ -17,5 +18,15 @@ describe('HeroComponent (shallow tests)', () => {
         fixture.componentInstance.hero = { id: 1, name: "SuperDude", strength: 3 };
 
         expect(fixture.componentInstance.hero.name).toEqual("SuperDude");
+    });
+
+    it('should render the hero name in an anchor tag', () => {
+        fixture.componentInstance.hero = { id: 1, name: "SuperDude", strength: 3 };
+        fixture.detectChanges();
+
+        let deA = fixture.debugElement.query(By.css('a'));
+        expect(deA.nativeElement.textContent).toContain('SuperDude');
+        // Alternate way to do the same expectation.
+        expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
     });
 });
